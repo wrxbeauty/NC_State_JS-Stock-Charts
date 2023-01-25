42bedb4e022608bd716f4a4ca3dae5e439d95e2f
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-01-25 09:57:42 -0500

    second commit

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+const { Chart } = require("chart.js");
+
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
@@ -12,10 +14,46 @@ async function main() {
     let DIS = result.DIS
     let BTNX = result.BTNX
 
+    const { GME, MSFT, DIS, BNTX } = mockData;
     const stocks = [GME, MSFT, DIS, BNTX];
-    const result = await response.json()
+    console.log(Chart)
 
+    const result = await response.json()
     console.log(result)
+
+    stocks.forEach(stock => stock.values.reverse())
+
+    // Chart
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.map(value => value.datetime),
+        datasets: stocks.map(stock => ({
+            label: stock.meta.symbol,
+            data: stock.values.map(calue => parseFloat(value.high)),
+            backgroundColor: getColor(stock.meta.symbol),
+            borderColor: getColor(stock.meta.symbol),
+        })
+    )}, 
+});
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+console.log(stocks[0].values)                                                  
+
 }
 
 
