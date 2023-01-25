e9e9e0d575e4053083d9ebfdef2b8ab47cd5530d
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-01-25 09:29:33 -0500

    first commit

--- public/index.js
+++ public/index.js
@@ -4,6 +4,20 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const response = await fetch ('https://api.twelvedata.com/time_series?symbol=GME,AMC,AAPL,LCID&interval=1day&apikey=')
+    console.log(response)
+
+    let GME = result.GME
+    let MSFT = result.MSFT
+    let DIS = result.DIS
+    let BTNX = result.BTNX
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+    const result = await response.json()
+
+    console.log(result)
 }
 
+
 main()
+
